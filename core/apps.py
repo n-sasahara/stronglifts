cee7e6cb5f444401d2e7bf4c11417df8b2426c14
sasahara <n.sasahara@proud-g.jp>
2021-04-23 18:04:57 +0900

    エクササイズの登録状況に合わせてフラグを変更する機能を実装

--- core/apps.py
+++ core/apps.py
@@ -6,7 +6,8 @@ class CoreConfig(AppConfig):
     name = 'core'
 
     def ready(self):
-        from core.signals import create_default_exercises, create_user_profile
-        from .models import User
+        from core.signals import create_default_exercises, create_user_profile, change_exercise_flag
+        from .models import User, ExerciseLog
         post_migrate.connect(create_default_exercises, sender=self)
         post_save.connect(create_user_profile, sender=User)
+        post_save.connect(change_exercise_flag, sender=ExerciseLog)
